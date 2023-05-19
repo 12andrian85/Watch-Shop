38fc58eb766b37f55ac696ba911cf678c6058a64
12andrian85 <118369529+12andrian85@users.noreply.github.com>
2023-05-19 18:24:47 +0300

    Co-authored-by: Artiom  <artiomsinigur@users.noreply.github.com>

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var myCarousel = document.querySelector('#myCarousel');
+var carousel = new bootstrap.Carousel(myCarousel);
+
+new Splide("#carousel", {
+    type: 'loop',
+    perPage: 1,
+    arrows: false,
+    pagination: false,
+    autoplay: true,
+    interval: 1500,
+}).mount();
